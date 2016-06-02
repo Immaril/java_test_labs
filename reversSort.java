825f57b7bb69218ce22377cd04db014fc539a375
Immaril <firewinged@yandex.ru>
2016-06-02 15:41:12 +0300

    Update reversSort.java

--- reversSort.java
+++ reversSort.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
+
 
 /* Массив из чисел в обратном порядке
 1. Создать массив на 10 чисел.
@@ -15,12 +14,27 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
         //напишите тут ваш код
-        Integer[] array=new Integer[10];
+        int[] array=new int[10];
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         for (int i=0; i<array.length;i++)
             array[i]=Integer.parseInt(reader.readLine());
         reader.close();
-        Arrays.sort(array, Collections.reverseOrder());
+        revers(array);
+        printArray(array);
+    }
+    private static int[] revers(int[] array)
+    {
+        int temp=0;
+        for (int i=0; i<array.length/2;i++)
+        {
+            temp = array[i];
+            array[i] = array[9-i];
+            array[9-i]=temp;
+        }
+        return array;
+    }
+    private static void printArray(int[]array)
+    {
         for (int i=0; i<array.length;i++)
             System.out.println(array[i]);
     }
