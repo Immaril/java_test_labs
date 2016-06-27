6cbf3711a8f6488967c348e45e0d10ff10f1989b
Immaril <firewinged@yandex.ru>
2016-06-27 12:16:40 +0300

    Create iSnovaKotegi.java

---
+++ iSnovaKotegi.java
@@ -0,0 +1,79 @@
+package com.javarush.test.level07.lesson12.bonus01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* Задача:  Программа вводит с клавиатуры данные про котов и выводит их на экран. Пример:
+Cat name is Barsik, age is 6, weight is 5, tail = 22
+Cat name is Murka, age is 8, weight is 7, tail = 20
+*/
+
+public class Solution
+{
+    public final static ArrayList<Cat> CATS = new ArrayList<Cat>();
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true)
+        {
+            System.out.print("Введите имя кота или оставьте пустым для завершения ввода: ");
+            String name = reader.readLine();
+            System.out.println("");
+            if (name.isEmpty()) break;
+            try
+            {
+                System.out.print("Введите возраст: ");
+                int age = Integer.parseInt(reader.readLine());
+                System.out.println("");
+                System.out.print("Введите вес: ");
+                int weight = Integer.parseInt(reader.readLine());
+                System.out.println("");
+                System.out.print("Введите длину ховста: ");
+                int tailLenght = Integer.parseInt(reader.readLine());
+                System.out.println("");
+                Cat cat = new Cat(name, age, weight, tailLenght );
+                CATS.add(cat);
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.print("Все значения, кроме имени, должны быть числовыми. Введите еще раз.");
+                System.out.println("");
+            }
+        }
+
+        printList();
+    }
+
+    public static void printList() {
+        for (int i = 0; i < CATS.size(); i++)
+        {
+            System.out.println(CATS.get(i));
+        }
+    }
+
+    public static class Cat
+    {
+        private String name;
+        private int age;
+        private int weight;
+        private int tailLength;
+
+        Cat(String name, int age, int weight, int tailLength)
+        {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.tailLength = tailLength;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "Cat name is " + name + ", age is " + age + ", weight is " + weight + ", tail = " + tailLength;
+        }
+    }
+}
